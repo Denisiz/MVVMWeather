bbc42ada99baade888f84b42bfd8c0247c99c90f
Denisiz <denis.izotov.1994@mail.ru>
2022-03-16 23:21:07 +0300

    temp

--- MVVMWeather/MVVMWeather/ContentView.swift
+++ MVVMWeather/MVVMWeather/ContentView.swift
@@ -13,7 +13,14 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack{
-                
+                Text(viewModel.timezone)
+                    .font(.system(size: 32))
+                Text(viewModel.temp)
+                    .font(.system(size: 44))
+                Text(viewModel.title)
+                    .font(.system(size: 24))
+                Text(viewModel.descriptionText)
+                    .font(.system(size: 24))
             }
             .navigationTitle("Погода в Курске")
         }
--- MVVMWeather/MVVMWeather/ViewModel.swift
+++ MVVMWeather/MVVMWeather/ViewModel.swift
@@ -10,8 +10,21 @@ import Foundation
 ///Данные для представления
 
 class WeatherViewModel: ObservableObject {
-    @Published var title: String = ""
-    @Published var descriptionText: String = ""
-    @Published var temp: String = ""
-    @Published var timezone: String = ""
+    @Published var title: String = "_"
+    @Published var descriptionText: String = "_"
+    @Published var temp: String = "_"
+    @Published var timezone: String = "_"
+    
+    /// Вызываем апи
+    init () {
+        fetchWeather()
+        
+    }
+    func fetchWeather() {
+        guard let url = URL(string: "")
+        let task = URLSession.shared.dataTask(with: url) {data, _, error in
+            
+        }
+        task.resume()
+    }
 }
